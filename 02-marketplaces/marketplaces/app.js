6207ea9dbe69c264a8618a4b8508ab9fc99de3d7
Ted Roden <tedroden@gmail.com>
2016-02-03 21:05:23 -0500

    setting up 02

---
+++ 02-marketplaces/marketplaces/app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var handlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home', { pageTitle: "Home" } );
+});
+
+
+app.get('/new', function (req, res) {
+    res.render('new', { pageTitle: "Submit a new task" } );
+});
+
+app.post('/new', function (req, res) {
+	console.log(req.body.title);
+
+	var data =  { title: req.body.title,
+				  body: req.body.body,
+				  price: req.body.price };
+	console.log(data);
+
+    res.render('new', { pageTitle: "Submit a new task", data: data } );
+});
+
+
+app.get('/list', function (req, res) {
+	var foods = [ "Beans", "Rice", "Pickles", "PB&J", "Nachos" ];
+	console.log(req.body.title);
+    res.render('list', { foods: foods, pageTitle: "My Favorite Foods" } );
+});
+
+app.listen(3000, function () {
+  console.log('Marketplace app listening on port 3000!');
+});
