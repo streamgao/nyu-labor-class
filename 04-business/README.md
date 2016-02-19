a9ebf9da9550e0cb6d40b907d82a5ffd8f3b5ba3
Ted Roden <tedroden@gmail.com>
2016-02-18 22:05:58 -0500

    adding a persistant storage interface

---
+++ 04-business/README.md
@@ -0,0 +1,33 @@
+## The Business Side
+
+* Demo assignments from last week
+* Discuss the Readings
+   * [When Your Boss Is an Uber Algorithm](https://www.technologyreview.com/s/543946/when-your-boss-is-an-uber-algorithm/)
+   * [In Praise of Efficient Price Gouging](https://www.technologyreview.com/s/529961/in-praise-of-efficient-price-gouging/)
+* Introduce the [Final Project]()
+* Building your own workforce
+  * Node persist `npm install node-persist --save`
+  * 
+
+   
+## Assignment
+
+* Email me an informal project proposal...
+  * What are you going to build? 
+  * What are the different parts? 
+    * Example using your own labor pool
+      * The `client` submits a form with collecting information needed
+      * The information is displayed as a list for the `worker` to choose from
+      * The `worker` selects a `job`, has some way to complete it
+      * Your code takes the results, combines them, and displays the `product`
+    * Example using a 3rd party labor pool
+      * The `client` submits a form with XYZ info
+      * That info gets sent to a Fancy Hands `worker` to do the `job`
+      * Your code takes the results, combines them, and displays the `product`
+	  
+* Prepare a final project proposal
+  * Next week, you'll talk about what we can expect to see in 
+
+
+
+
---
+++ 04-business/business/app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var handlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
+var storage = require('node-persist'); 
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// setup the storage...
+storage.initSync();
+app.set('storage', storage);
+
+app.get('/', function (req, res) {
+    res.render('home', { pageTitle: "Home", name: req.query.name } );
+});
+
+// Display a new form (located in the file views/new.handlebars)
+app.get('/new', function (req, res) {
+	// laod the message from storage
+	var message = storage.getItem('message');
+    res.render('new', { pageTitle: "Submit a new task", message: message } );
+});
+
+// Accept a POST request and display it (template in the file views/new.handlebars)
+app.post('/new', function (req, res) {
+	// set the latest message
+	storage.setItem("message", req.body.message);
+
+	// load the existing messages
+	var existing_messages = storage.getItem("message-list");
+
+	// no existing items? create an empty list
+	if(!existing_messages) {
+		existing_messages = [];
+	}
+
+	// save the message to the list of existing messages
+	existing_messages.push(req.body.message);
+	storage.setItem("message-list", existing_messages);
+
+	// redirect back to the main page
+	res.redirect("/new");
+});
+
+// This creates a list of `foods` and displays it in a new template
+// template located (views/list.handlebars)
+app.get('/list', function (req, res) {
+	var messages = storage.getItem("message-list");
+    res.render('list', { messages: messages, pageTitle: "Existing messages" } );
+});
+
+app.listen(3000, function () {
+  console.log('Marketplace app listening on port 3000!');
+});
